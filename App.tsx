fa9726845b028be05e0530e6e9835a8c09b92181
Rishav <rishav@Rishavs-Mac-mini.local>
2026-01-05 15:30:05 +0530

    Updated git ignore file

--- App.tsx
+++ App.tsx
@@ -1,45 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { I18nextProvider } from 'react-i18next';
+import { LogBox, StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import i18n from './src/i18n';
+import Welcome from './src/screens/welcome';
+import AuthNavigation from './src/navigation/AuthNavigation';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+LogBox.ignoreAllLogs();
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(): React.JSX.Element {
+  // const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+    <SafeAreaProvider style={styles.container}>
+      <NavigationContainer>
+        <I18nextProvider i18n={i18n}>
+          {/* <Welcome /> */}
+          <AuthNavigation />
+        </I18nextProvider>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: { flex: 1 },
 });
 
 export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+//   plugins: [['react-native-worklets/plugin']],
+// };
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [['react-native-worklets/plugin']],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        allowUndefined: true,
+      },
+    ],
+    ['react-native-worklets/plugin'], // keep this LAST
+  ],
 };
